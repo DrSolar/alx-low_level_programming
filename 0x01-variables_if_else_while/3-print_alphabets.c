4561d9267dd05a11e195fec9e47f2241693b1f11
Solarin Sodiq <sodiqsolarin59@gmail.com>
2022-06-06 16:11:56 -0700

    fgg

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,30 +9,22 @@
 int main(void)
 
 {
-
-		char a = 'a';
-
-
-
-			while (a <= 'z')
-
-					{
-						putchar(a);
-						a++;
-
-											}
-			a = 'A';
-
-					while (a <= 'Z')
-
-							{
-								putchar(a);
-								a++;
-
-													}
-
-						putchar('\n');
-
-							return (0);
+	char a = 'a';
+	
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	
+	a = 'A';
+	while (a <= 'Z')
+	{
+		putchar(a);
+		a++;
+	}
+	
+	putchar('\n')
+	return (0);
 
 }
