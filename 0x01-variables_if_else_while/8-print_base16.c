96cbcbee68a907641b42f3b90c8ccc2c1fb30a12
Solarin Sodiq <sodiqsolarin59@gmail.com>
2022-06-06 17:08:48 -0700

    ggghjk

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,11 +2,12 @@
 /**
  *  * main - prints out all the numbers of base 16 in lowercase
  *   * Return: 0
- *    */
+ **/
 int main(void)
 {
 	char b;
-	
+
+
 	for (b = 0; b < 10; b++)
 		putchar(b + '0');
 	for (b = 'a'; b <= 'f'; b++)
