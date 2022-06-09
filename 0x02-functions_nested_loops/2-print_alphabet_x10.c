dc22a530e365ef6ac0f422ad1cff66e6bec34262
Solarin Sodiq <sodiqsolarin59@gmail.com>
2022-06-08 20:52:51 -0700

    ll

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- *
  *  * print_alphabet_x10 - Print alphabet in lowercase 10 times
- *
  *   * Return: 0
- *
  **/
 
 void print_alphabet_x10(void)
@@ -13,6 +10,7 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	char c;
+
 	while (i < 10)
 
 	{
