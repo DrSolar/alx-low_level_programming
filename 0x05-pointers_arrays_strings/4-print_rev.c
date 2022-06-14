6df71f43cc832fd3e1f9bf05b892fd403d021a36
Solarin Sodiq <sodiqsolarin59@gmail.com>
2022-06-13 21:51:03 -0700

    jgjh

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -19,15 +19,13 @@ void print_rev(char *s)
 
 
 
-			while (s[i++])
-
-						len++;
+			while (s[i++]
+					len++;
 
 
 
 				for (i = len - 1; i >= 0; i--)
-
-							_putchar(s[i]);
+				_putchar(s[i]);
 
 
 
