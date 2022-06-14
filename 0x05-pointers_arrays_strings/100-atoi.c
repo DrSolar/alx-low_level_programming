af8cf1fb591b4ebbde64b4848418355a201aae04
Solarin Sodiq <sodiqsolarin59@gmail.com>
2022-06-13 23:58:39 -0700

    dfkk

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,14 +3,13 @@
 
 
 /**
- *
  *  * _atoi - Converts a string to an integer.
  *
  *   * @s: The string to be converted.
  *
  *    * Return: The integer value of the converted string.
  *
- *     */
+ **/
 
 
 
